f7bd47c1c371a35f094f8d0d6463767cc46207ce
shaw-samina <jinxcapsule@yahoo.com>
2026-06-17 00:40:21 -0400

    Added github repo URL

--- weather-app-project/src/App.jsx
+++ weather-app-project/src/App.jsx
@@ -3,5 +3,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return "weather";
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by{" "}
+        <a href="https://github.com/shaw-samina">Samina Shaw </a>
+        and is open-sourced on {""}
+        <a
+          href="https://github.com/shaw-samina/react-weather-app-ss"
+          target="_blank"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
